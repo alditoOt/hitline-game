408d6f20d9da99f80a3ccfca9b33359d98d9aa97
Aldo Oporto <a_r_oporto@outlook.com>
2021-07-09 17:00:36 -0400

    added enemy follow, retreat and shooting

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float approachDistance;
+    public float retreatDistance;
+
+    private Transform target;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector2 lookDir = target.position - transform.position;
+        if(Vector2.Distance(transform.position, target.position) > approachDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position, target.position) < approachDistance && Vector2.Distance(transform.position, target.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if(Vector2.Distance(transform.position, target.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+        }
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        Debug.Log(Vector2.Distance(transform.position, target.position));
+    }
+}
---
+++ Assets/EnemyShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullet;
+    public float bulletForce = 20f;
+    public float startingFireRate = 3f;
+    private float fireRate;
+
+    private void Start()
+    {
+        fireRate = startingFireRate;
+    }
+
+    private void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if(fireRate <= 0)
+        {
+            fireRate = startingFireRate;
+            GameObject bullet = Instantiate(this.bullet, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            fireRate -= Time.deltaTime;
+        }
+    }
+}
