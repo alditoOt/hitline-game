70d0fc90bb555d8e282124193fa3f3b7d70f3367
Aldo Oporto <a_r_oporto@outlook.com>
2021-07-10 01:25:30 -0400

    modified character sprites

---
+++ Assets/Scripts/{}Enemies/EnemyPlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayerCollision : MonoBehaviour
+{
+    private void Start()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponents<Collider2D>();
+        for(int i = 0; i < player.Length; i ++)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player[i], true);
+        }
+    }
+}
