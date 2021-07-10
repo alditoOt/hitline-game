6ddd77a6196f024fd351977cbeca716e425ac9ae
Aldo Oporto <a_r_oporto@outlook.com>
2021-07-10 00:14:34 -0400

    fixed problem where the enemy would slide off screen when dead

---
+++ Assets/EnemyPlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayerCollision : MonoBehaviour
+{
+    private Collider2D player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player, true);
+    }
+}
--- Assets/Scripts/{}Enemies/EnemyDead.cs
+++ Assets/Scripts/{}Enemies/EnemyDead.cs
@@ -6,10 +6,12 @@ public class EnemyDead : MonoBehaviour
 {
     private Animator anim;
     private PolygonCollider2D pc;
+    private Rigidbody2D rb;
     private void Start()
     {
         anim = GetComponent<Animator>();
         pc = GetComponent<PolygonCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +21,7 @@ public class EnemyDead : MonoBehaviour
             anim.SetBool("Dead", true);
             Destroy(collision.gameObject);
             pc.enabled = false;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 }
