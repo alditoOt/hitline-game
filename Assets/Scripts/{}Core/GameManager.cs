1d414ef36a4544da593690e8515fd846e93b45f1
Aldo Oporto <a_r_oporto@outlook.com>
2021-07-11 02:12:58 -0400

    added door, and collider for the player to not directly touch the walls

--- Assets/Scripts/{}Core/GameManager.cs
+++ Assets/Scripts/{}Core/GameManager.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
     private static int enemyCount;
+    private GameObject openingWall;
     private void Start()
     {
         enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-       
+        openingWall = GameObject.FindGameObjectWithTag("OpeningWall");
     }
     public void EnemyDead()
     {
         enemyCount--;
         if(enemyCount <= 0)
         {
+            openingWall.SetActive(false);
             Debug.Log("OH MY GOD EVERYONE'S DEAD");
         }
     }
