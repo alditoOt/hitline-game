53f0e360431d27f3641a23898573979a15b35d5d
Aldo Oporto <a_r_oporto@outlook.com>
2021-07-11 23:30:20 -0400

    fixed enemy death sprite

--- Assets/Scripts/{}Player/PlayerDead.cs
+++ Assets/Scripts/{}Player/PlayerDead.cs
@@ -7,16 +7,22 @@ public class PlayerDead : MonoBehaviour
 {
     private PlayerMovement movementScript;
     public bool dead = false;
+    private Animator anim;
+    private Rigidbody2D rb;
 
     private void Start()
     {
         movementScript = GetComponent<PlayerMovement>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("EnemyBullet"))
         {
             dead = true;
+            anim.SetBool("Dead", true);
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
             movementScript.moveSpeed = 0;
             movementScript.mousePosition = new Vector2(0f, 0f);
             Destroy(collision.gameObject);
